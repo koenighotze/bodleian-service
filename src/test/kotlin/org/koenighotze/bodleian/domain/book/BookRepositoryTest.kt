af6335dece56d808a15901e0469edbeaa28a8732
David Schmitz <davidschmitz@posteo.de>
2023-09-27 21:12:39 +0200

    WIP

---
+++ src/test/kotlin/org/koenighotze/bodleian/domain/book/BookRepositoryTest.kt
@@ -0,0 +1,25 @@
+package org.koenighotze.bodleian.domain.book
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.koenighotze.bodleian.book.Book
+import org.koenighotze.bodleian.book.Book.Companion.randomId
+import org.koenighotze.bodleian.book.BookRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+
+//TODO mit testcontainer
+@DataJpaTest
+class BookRepositoryTest(@Autowired var repository: BookRepository) {
+
+    @Test
+    fun `The repository returns an empty list of books`() {
+        assertThat(repository.findAll()).isEmpty()
+    }
+
+    fun `a stored book is returned`() {
+        repository.save(Book(title = "Some title", authors = "Some author", id = randomId()))
+
+        assertThat(repository.findAll()).isNotEmpty
+    }
+}
