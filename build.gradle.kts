80f0f01603a0e9d279fbafb0e8ff4dad191ab07b
David Schmitz <david.schmitz.privat@gmail.com>
2023-10-04 17:30:39 +0200

    cleanup

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ import java.lang.Runtime.getRuntime
 
 plugins {
     id("org.springframework.boot") version "3.1.4"
-//    id("io.spring.dependency-management") version "1.1.3"
+    id("io.spring.dependency-management") version "1.1.3"
     kotlin("jvm") version "1.8.22"
     kotlin("plugin.spring") version "1.8.22"
     kotlin("plugin.jpa") version "1.8.22"
@@ -16,7 +16,7 @@ plugins {
 
 }
 
-apply(plugin = "io.spring.dependency-management")
+//apply(plugin = "io.spring.dependency-management")
 
 group = "org.koenighotze"
 version = "0.0.1-SNAPSHOT"
