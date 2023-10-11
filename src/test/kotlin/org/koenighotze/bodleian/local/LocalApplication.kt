59ecd8dca5d1e22d263f6956c319b73420fb4365
David Schmitz <david.schmitz.privat@gmail.com>
2023-10-11 16:32:53 +0300

    Setup postgres with fixed ports

--- src/test/kotlin/org/koenighotze/bodleian/local/LocalApplication.kt
+++ src/test/kotlin/org/koenighotze/bodleian/local/LocalApplication.kt
@@ -1,5 +1,9 @@
 package org.koenighotze.bodleian.local
 
+import com.github.dockerjava.api.model.ExposedPort
+import com.github.dockerjava.api.model.HostConfig
+import com.github.dockerjava.api.model.PortBinding
+import com.github.dockerjava.api.model.Ports
 import org.koenighotze.bodleian.Application
 import org.springframework.boot.fromApplication
 import org.springframework.boot.test.context.TestConfiguration
@@ -12,8 +16,25 @@ import org.testcontainers.containers.PostgreSQLContainer
 internal class LocalApplicationConfiguration {
     @Bean
     @ServiceConnection
-    fun postgresContainer() =
-        PostgreSQLContainer("postgres:16.0-alpine3.18")
+    fun postgresContainer(): PostgreSQLContainer<out PostgreSQLContainer<*>> {
+        val containerPort = 5432
+        val localPort = 5432
+
+        val container = PostgreSQLContainer("postgres:16.0-alpine3.18")
+            .withDatabaseName("bodleian")
+            .withUsername("postgres")
+            .withPassword("postgres")
+            .withReuse(true)
+            .withExposedPorts(containerPort)
+            .withCreateContainerCmdModifier {
+                it.withHostConfig(
+                    HostConfig.newHostConfig()
+                        .withPortBindings(PortBinding(Ports.Binding.bindPort(localPort), ExposedPort(containerPort)))
+                )
+            }
+        container.start()
+        return container
+    }
 }
 
 fun main(args: Array<String>) {
